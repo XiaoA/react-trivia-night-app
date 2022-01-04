2d0e8855058c7ba23663e14588797532f4ea5c0c
Andrew Buckingham <abuckingham99@gmail.com>
2022-01-03 20:10:36 -0800

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders title', () => {
+  const {getByText } = render(<App />);
+  const title = screen.getByText(/Trivia Night/i);
+  expect(title).toBeInTheDocument();
 });
