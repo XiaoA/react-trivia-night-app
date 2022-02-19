ae889f9bee34f9bec21a498ccabeb21152746ac5
Andrew Buckingham <abuckingham99@gmail.com>
2022-02-19 12:33:14 -0800

    Remove {auth} import from Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { Link } from "react-router-dom";
-import { useAuth } from '../contexts/AuthContext';
 
 const Login = ({ handleLogin, history, isLoggedIn }) => {
   const [email, setEmail] = useState('');
